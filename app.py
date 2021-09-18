c199815dc8a920ed9dbdea15ceea053b63a68141
kennethtansg <kenneth.tansg@gmail.com>
2021-09-18 14:45:49 +0800

    1.0.1

---
+++ app.py
@@ -0,0 +1,49 @@
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import numpy as np
+import cv2
+#Initialize the Flask app
+app = Flask(__name__)
+
+#camera = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+def gen_frames():  
+        while True:
+            ret, frame = cap.read()
+                   
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+                roi_gray = gray[y:y+w, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+#            cv2.imshow('frame', frame)
+
+#            if cv2.waitKey(1) == ord('q'):
+#                break
+#        cap.release()
+#        cv2.destroyAllWindows()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
